11098d7e25d41ab0e29e4a766bf63b0008723221
Stefano Crespi <Stefano.Crespi@accuity.com>
2015-11-17 10:52:56 -0600

    Some lambdas.

--- src/main/kotlin/koans/Koans.kt
+++ src/main/kotlin/koans/Koans.kt
@@ -9,3 +9,8 @@ fun start() = "OK"
 
 // named - default arguments
 fun namedArguments(paramA: String = "a", paramB: Int = 3): String = paramA + paramB.toString()
+
+// lambdas
+fun containsEven(collection: Collection<Int>): Boolean = collection.any { x -> x % 2 == 0 }
+fun evenPredicate(x: Int): Boolean = x % 2 == 0
+fun filterOdds(collection: Collection<Int>): Collection<Int> = collection.filter(::evenPredicate)
--- src/test/kotlin/koans/KoansTest.kt
+++ src/test/kotlin/koans/KoansTest.kt
@@ -9,19 +9,28 @@ import org.junit.Test as test
 
 class TestKoans {
     @test fun testOk() {
-        Assert.assertEquals("Returned string should match","OK", start())
+        Assert.assertEquals("Returned string should match", "OK", start())
     }
 
     @test fun testNamedArguments() {
-        Assert.assertEquals("Returned string should match","c4", namedArguments(paramB = 4, paramA = "c"))
+        Assert.assertEquals("Returned string should match", "c4", namedArguments(paramB = 4, paramA = "c"))
     }
 
     @test fun testDefaultArguments() {
-        Assert.assertEquals("Returned string should match","a3", namedArguments())
+        Assert.assertEquals("Returned string should match", "a3", namedArguments())
     }
 
     @test fun testMixedArguments() {
-        Assert.assertEquals("Returned string should match","a5", namedArguments(paramB = 5))
-        Assert.assertEquals("Returned string should match","v7", namedArguments("v",7))
+        Assert.assertEquals("Returned string should match", "a5", namedArguments(paramB = 5))
+        Assert.assertEquals("Returned string should match", "v7", namedArguments("v",7))
+    }
+
+    @test fun testLambda() {
+        Assert.assertEquals("Returned boolean should match", true, containsEven(listOf(1, 2, 3)))
+    }
+
+    @test fun testNamedLambda() {
+        Assert.assertEquals("Returned collection should match", 1, filterOdds(listOf(1, 2, 3)).size)
+        Assert.assertEquals("Returned boolean should match", true, containsEven(filterOdds(listOf(1, 2, 3))))
     }
 }
